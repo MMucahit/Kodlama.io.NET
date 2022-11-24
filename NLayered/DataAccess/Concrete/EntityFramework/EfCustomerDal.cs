ebf248cd72d170a6de560cad8f3848c4b5143feb
MMucahitNAS <MMucahitNAS@gmail.com>
2022-11-24 17:00:54 +0300

    EfEntityRepositoryBase has been added and EntityFramework which is in DataAccess layer has been refactoring according to EfEntityRepositoryBase

--- NLayered/DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
+++ NLayered/DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -9,31 +10,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCustomerDal : ICustomerDal
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, NorthwindContext>, ICustomerDal
     {
-        public void Add(Customer entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Customer entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Customer Get(Expression<Func<Customer, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Customer> GetAll(Expression<Func<Customer, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Customer entity)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
