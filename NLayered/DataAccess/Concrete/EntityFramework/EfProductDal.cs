4d74276a3597ce559051a449f4744691c9832491
MMucahitNAS <MMucahitNAS@gmail.com>
2022-11-24 14:25:48 +0300

    EntityFramework Operations has been created

--- NLayered/DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ NLayered/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,27 +14,56 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            // Using bloğu bitince context nesnesi bellekden silinir.
+            // IDisposable pattern implementasyonu of C#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                // Referansı yakala
+                var addedEntity = context.Entry(entity);
+                // Yapılacak işlemi seçme
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+
         }
 
         public Product Get(Expression<Func<Product, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
         }
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                // ternary  
+                return filter==null 
+                    ? context.Set<Product>().ToList()
+                    :context.Set<Product>().Where(filter).ToList(); 
+            }
         }
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
